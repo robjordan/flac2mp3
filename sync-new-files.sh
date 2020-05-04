92592cd0a5631195a60e36ccb39f46fb24d3c811
Rob Jordan <github@jordan-maynard.com>
2020-05-04 15:30:29 +0100

    elderly updates

--- sync-new-files.sh
+++ sync-new-files.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export BASE=/media/music-rw
-export lockfile=$BASE/portable/sync-new-files-running
+export lockfile=$HOME/flac2mp3/sync-new-files-running
+export lastupdated=$HOME/flac2mp3/last-updated
 
 cd $BASE/master
 # make a link in 'portable' to any new/changed files in 'master' (exit if another iteration is still running)
@@ -9,10 +10,10 @@ if [ -f "$lockfile" ]; then
   exit 1
 fi
 touch "$lockfile"
-find . -type f -newer ../portable/last-updated -print | cpio --quiet -pdmlv ../portable
+find . -type f -newer "$lastupdated" -print | cpio --quiet -pdmlv ../portable
 
 # rerun the FLAC->mp3 conversion process
 find $BASE/portable -name '*.flac' -exec $HOME/bin/flac2mp3.sh {} \;
 
 # finally update the timestamp for next time
-mv "$lockfile" $BASE/portable/last-updated
+mv "$lockfile" "$lastupdated"
